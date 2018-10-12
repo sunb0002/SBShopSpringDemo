f6274d9c99105e670751674043409a7ffc8fd49b
sun.bo.mac <st_sunbo@hotmail.com>
2018-10-13 01:23:06 +0800

    LCS part2

--- src/test/java/com/madoka/sunb0002/webapi/LongestCommonSequence.java
+++ src/test/java/com/madoka/sunb0002/webapi/LongestCommonSequence.java
@@ -24,6 +24,7 @@ public class LongestCommonSequence {
 		String C = "Madoka";
 
 		log.info("Longest Common Substring: {}", longestCommonSubstring(A, C));
+		log.info("Longest Common Substring: {}", longestCommonSequence(B, C));
 
 	}
 
@@ -77,7 +78,65 @@ public class LongestCommonSequence {
 	 * @return Longest common sequence
 	 */
 	public static String longestCommonSequence(String strA, String strB) {
-		return "TBD";
+
+		if (Validators.isEmpty(strA) || Validators.isEmpty(strB)) {
+			return "";
+		}
+
+		int[][] count = new int[strA.length()][strB.length()];
+		int maxLength = 0, iMax = 0, jMax = 0;
+
+		for (int i = 0; i < strA.length(); i++) {
+			for (int j = 0; j < strB.length(); j++) {
+
+				char chA = strA.charAt(i);
+				char chB = strB.charAt(j);
+
+				if (chA == chB) {
+					count[i][j] = (i == 0 || j == 0) ? 1 : count[i - 1][j - 1] + 1;
+					// Record the max value coordinate.
+					if (count[i][j] > maxLength) {
+						maxLength = count[i][j];
+						iMax = i;
+						jMax = j;
+					}
+				} else {
+					count[i][j] = Math.max((i > 0) ? count[i - 1][j] : 0, (j > 0) ? count[i][j - 1] : 0);
+				}
+			}
+		}
+
+		return printLCS(count, strA, iMax, jMax);
+	}
+
+	/**
+	 * Recursively print LCS string.
+	 * 
+	 * @param countMatrix
+	 * @param strA
+	 * @param i
+	 * @param j
+	 * @return
+	 */
+	private static String printLCS(final int[][] countMatrix, String strA, int i, int j) {
+
+		int iCurrent = countMatrix[i][j];
+
+		if (iCurrent == 1) {
+			return strA.charAt(i) + "";
+		} else if (i == 0 || j == 0) {
+			return "";
+		}
+
+		int iPrev = countMatrix[i - 1][j];
+		int jPrev = countMatrix[i][j - 1];
+
+		if (iPrev == jPrev) {
+			return printLCS(countMatrix, strA, i - 1, j - 1) + strA.charAt(i);
+		} else {
+			return (iPrev < jPrev) ? printLCS(countMatrix, strA, i, j - 1) : printLCS(countMatrix, strA, i - 1, j);
+		}
+
 	}
 
 }
